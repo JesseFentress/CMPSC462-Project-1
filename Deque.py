158f4cbb8a8e208c7b9cd43d57e853e1b2e09e34
JesseFentress <61528881+JesseFentress@users.noreply.github.com>
2021-09-11 19:19:09 -0400

    deque calls from runestone academy

--- Deque.py
+++ Deque.py
@@ -2,3 +2,23 @@ class Deque:
 
     def __init__(self):
         self.list = []
+
+    def is_Empty(self):
+        return self.list == []
+
+    def size(self):
+        return len(self.list)
+
+    def addFront(self, item):
+        self.list.append(item)
+
+    def addRear(self, item):
+        self.list.insert(0, item)
+
+    def removeFront(self):
+        return self.list.pop()
+
+    def removeRear(self):
+        return self.list.pop(0)
+
+
